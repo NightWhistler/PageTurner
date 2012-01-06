9c978dec4f325fa026c930eb8c697d308f19b70b
Alex Kuiper <alex@nightwhistler.net>
2012-01-06 16:20:39 +0100

    Added license info and credits.

--- src/main/java/net/nightwhistler/pageturner/animation/PageCurlAnimator.java
+++ src/main/java/net/nightwhistler/pageturner/animation/PageCurlAnimator.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011 Alex Kuiper
+ * 
+ * This file is part of PageTurner
+ *
+ * PageTurner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PageTurner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PageTurner.  If not, see <http://www.gnu.org/licenses/>.*
+ */
 package net.nightwhistler.pageturner.animation;
 
 import android.graphics.Bitmap;
@@ -9,7 +27,17 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.text.TextPaint;
 
-
+/**
+ * PageCurl animator, simulates flipping pages.
+ * 
+ * All credit for this goes to Moritz 'Moss' Wundke (b.thax.dcg@gmail.com),
+ * since it's just an adapted and simplified version of his PageCurlView.
+ * 
+ * The original project is here: http://code.google.com/p/android-page-curl/
+ *
+ * @author Alex Kuiper
+ *
+ */
 public class PageCurlAnimator implements Animator {
 		
 	

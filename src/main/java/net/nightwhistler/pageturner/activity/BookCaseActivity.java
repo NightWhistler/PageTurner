f31f2b4030c1af6f430d20ba5b767d517c259a5e
Alex Kuiper <alex@nightwhistler.net>
2012-02-14 23:01:39 +0100

    BookCase view mostly working.

--- src/main/java/net/nightwhistler/pageturner/activity/BookCaseActivity.java
+++ src/main/java/net/nightwhistler/pageturner/activity/BookCaseActivity.java
@@ -1,11 +1,12 @@
 package net.nightwhistler.pageturner.activity;
 
 import net.nightwhistler.pageturner.R;
-import net.nightwhistler.pageturner.animation.Animations;
 import net.nightwhistler.pageturner.library.LibraryBook;
 import net.nightwhistler.pageturner.library.LibraryService;
 import net.nightwhistler.pageturner.library.QueryResult;
-import net.nightwhistler.pageturner.view.BookCaseDrawable;
+import net.nightwhistler.pageturner.library.QueryResultAdapter;
+import net.nightwhistler.pageturner.view.BookCaseView;
+import net.nightwhistler.pageturner.view.BookCoverDrawable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,57 +15,33 @@ import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.GestureDetector;
-import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.MenuItem.OnMenuItemClickListener;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ViewSwitcher;
 
-import com.globalmentor.android.widget.VerifiedFlingListener;
 import com.google.inject.Inject;
 
 public class BookCaseActivity extends RoboActivity {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(BookCaseActivity.class);
 	
-	@InjectView(R.id.bookCaseImage)
-	private ImageView bookCaseView;
-	
-	@InjectView(R.id.bookCaseDummy)
-	private ImageView dummyCaseView;	
-	
-	@InjectView(R.id.bookCaseViewSwitcher)
-	private ViewSwitcher viewSwitcher;
-	
-	@InjectResource(R.drawable.pine)
-	private Drawable background;
-	
-	@InjectResource(R.drawable.shelf)
-	private Drawable shelf;
+	@InjectView(R.id.bookCaseView)
+	private BookCaseView bookCaseView;	
 	
 	@InjectResource(R.drawable.river_diary)
 	private Drawable fallBackCover;
-	
-	private GestureDetector gestureDetector;
 		
-	private BookCaseDrawable bookCaseDrawable;
-	private BookCaseDrawable emptyBookCase;
-	
-	private View.OnTouchListener gestureListener;
-	
 	@Inject
 	private LibraryService libraryService;
 	
@@ -72,126 +49,32 @@ public class BookCaseActivity extends RoboActivity {
 	
 	private ProgressDialog waitDialog;
 	
+	private BookViewAdapter adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 				
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.bookcase);
 		
-		this.bookCaseDrawable = new BookCaseDrawable();
-		this.bookCaseDrawable.setBackground(background);
-		this.bookCaseDrawable.setShelf(shelf);
-		this.bookCaseDrawable.setFallBackCover(fallBackCover);
-		
-		this.emptyBookCase = new BookCaseDrawable();
-		this.emptyBookCase.setBackground(background);
-		this.emptyBookCase.setShelf(shelf);
-		this.emptyBookCase.setFallBackCover(fallBackCover);
-		
-		this.bookCaseView.setImageDrawable(bookCaseDrawable);
-		this.bookCaseView.setBackgroundDrawable(emptyBookCase);
-		this.dummyCaseView.setBackgroundDrawable(emptyBookCase);
-		
-		this.gestureDetector = new GestureDetector(this, new ClickListener());
-        this.gestureListener = new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-            	LOG.debug("Got touch event: " + event );
-                return gestureDetector.onTouchEvent(event);
-            }
-        };   
+		//this.bookCaseView.setShelf(this.shelf);
+		//this.bookCaseView.setBackground(this.background);		
         
         this.waitDialog = new ProgressDialog(this);
         waitDialog.setOwnerActivity(this);
         
-    	this.viewSwitcher.setOnTouchListener(gestureListener);
-    	this.dummyCaseView.setOnTouchListener(gestureListener);
-    	this.bookCaseView.setOnTouchListener(gestureListener);	
-    	
-    	registerForContextMenu(viewSwitcher);
-    	registerForContextMenu(dummyCaseView);
-    	registerForContextMenu(bookCaseView);
-    	
-    	new LoadBooksTask().execute(3);
-		
-	}
-	
-	@Override
-    public boolean onTouchEvent(MotionEvent event) {    	
-    	return this.gestureDetector.onTouchEvent(event);
-    }
-	
-	@Override
-	public boolean dispatchKeyEvent(KeyEvent event) {
-		if ( event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN 
-				&& event.getAction() == KeyEvent.ACTION_DOWN ) {
-			switchPage(true);
-			return true;
-		}
-		
-		if ( event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP 
-				&& event.getAction() == KeyEvent.ACTION_DOWN ) {
-			switchPage(false);
-			return true;
-		}
-		
-		return false;
-	}
-	
-	public void switchPage( boolean right ) {
-		
-		int currentView = viewSwitcher.getDisplayedChild();		
-		int otherView = Math.abs( currentView - 1 );
-		
-		int newOffset = 0;
-		
-		if (right ) {	
-			viewSwitcher.setInAnimation( Animations.inFromRightAnimation() );
-			viewSwitcher.setOutAnimation(Animations.outToLeftAnimation() );				
-			
-			newOffset = bookCaseDrawable.getStartOffset()
-				+ bookCaseDrawable.getAmountOfBooks();
-			
-			if ( newOffset >= ( bookCaseDrawable.getBooks().getSize() -1) ) {
-				newOffset = 0;
-			}
-		} else {
-			viewSwitcher.setInAnimation(Animations.inFromLeftAnimation());
-			viewSwitcher.setOutAnimation(Animations.outToRightAnimation() );
-			
-			newOffset = bookCaseDrawable.getStartOffset()
-				- bookCaseDrawable.getCapacity();
-			
-			if ( newOffset < 0 ) {
-				newOffset = (bookCaseDrawable.getBooks().getSize() -1) - newOffset;
-			}
-		}		
-		
-		ImageView imageView = (ImageView) viewSwitcher.getChildAt(otherView);
-		imageView.setImageBitmap(null);
-		imageView.setImageDrawable(null);
-
-		this.bookCaseDrawable.getBooks().close();
-		this.bookCaseDrawable.setBooks( libraryService.findAllByTitle() );
-		this.bookCaseDrawable.setStartOffset(newOffset);
-		
-		try {
-			
-			Bitmap bitmap = Bitmap.createBitmap( bookCaseView.getWidth(),
-					bookCaseView.getHeight(), Config.ARGB_8888 );
-			Canvas canvas = new Canvas(bitmap);
-			this.bookCaseDrawable.setBounds( 0, 0, bookCaseView.getWidth(), bookCaseView.getHeight() );
-			this.bookCaseDrawable.draw(canvas);
-
-			imageView.setImageBitmap(bitmap);
-		} catch (OutOfMemoryError err) {
-			( (ImageView) viewSwitcher.getChildAt(currentView) ).setImageDrawable(null);
-			( (ImageView) viewSwitcher.getChildAt(currentView) ).setImageBitmap(null);
-			imageView.setImageBitmap(null);
-			imageView.setImageDrawable(bookCaseDrawable);	
-		}		
-		
-		viewSwitcher.showNext();
+        this.adapter = new BookViewAdapter(this);
+        bookCaseView.setAdapter(adapter);
+        
+        adapter.setResult(libraryService.findAllByTitle());        
+    		
+    	new LoadBooksTask().execute(3);		
 	}	
+	
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
 	
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
@@ -230,58 +113,49 @@ public class BookCaseActivity extends RoboActivity {
 
 	}	
 	
-	
-	private class ClickListener extends VerifiedFlingListener {
+	private class BookViewAdapter extends QueryResultAdapter<LibraryBook> {
 		
-		public ClickListener() {
-			super(BookCaseActivity.this);
+		private Context context;
+		
+		public BookViewAdapter(Context context) {
+			this.context = context;
 		}
 		
 		@Override
-		public void onLongPress(MotionEvent e) {
-			selectedBook = bookCaseDrawable.findBookAtLocation(e.getX(), e.getY());
-			openContextMenu(viewSwitcher);
-		}		
+		public View getView(int index, LibraryBook object, View convertView,
+				ViewGroup parent) {
+			
+			View result;
 		
-		@Override
-		public boolean onSingleTapConfirmed(MotionEvent e) {
-			LibraryBook book = bookCaseDrawable.findBookAtLocation(e.getX(), e.getY());
-			
-			if ( book != null ) {
-				Intent intent = new Intent(BookCaseActivity.this, ReadingActivity.class);
-			
-				intent.setData( Uri.parse(book.getFileName()));
-				setResult(RESULT_OK, intent);
+			if ( convertView == null ) {
 				
-				startActivityIfNeeded(intent, 99);
-			
-				return true;
+				LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+						Context.LAYOUT_INFLATER_SERVICE);
+				result = inflater.inflate(R.layout.bookcase_row, parent, false);
+				
+			} else {
+				result = convertView;
 			}
 			
-			return false;
-		}
-		
-		@Override
-		public boolean onVerifiedFling(MotionEvent e1, MotionEvent e2,
-				float velocityX, float velocityY) {
-						
-			LOG.debug("Fling confirmed.");
+			ImageView image = (ImageView) result.findViewById(R.id.bookCover);
 			
-			switchPage( velocityX < 0 );
 			
-			return true;
+			Bitmap bitmap = object.getCoverImage();
+			
+			if ( bitmap != null ) {			
+				image.setImageBitmap( object.getCoverImage() );
+			} else {
+				image.setImageDrawable(fallBackCover);
+			}		
+			
+			return result;
 		}
-		
 	}
-	
+
 	private class LoadBooksTask extends AsyncTask<Integer, Integer, QueryResult<LibraryBook>> {		
 		
 		@Override
-		protected void onPreExecute() {
-			
-			bookCaseDrawable.setDrawBooks(false);
-			bookCaseView.setImageDrawable(bookCaseDrawable);
-			viewSwitcher.setDisplayedChild(0);
+		protected void onPreExecute() {		
 			
 			waitDialog.setTitle("Loading library...");
 			waitDialog.show();
@@ -305,9 +179,8 @@ public class BookCaseActivity extends RoboActivity {
 		
 		@Override
 		protected void onPostExecute(QueryResult<LibraryBook> result) {
-			bookCaseDrawable.setBooks(result);
-			bookCaseDrawable.setDrawBooks(true);
-			viewSwitcher.invalidate();
+			adapter.setResult(result);
+			
 			waitDialog.hide();			
 		}
 		
--- src/main/java/net/nightwhistler/pageturner/view/BookCaseView.java
+++ src/main/java/net/nightwhistler/pageturner/view/BookCaseView.java
@@ -1,63 +1,44 @@
 package net.nightwhistler.pageturner.view;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import net.nightwhistler.pageturner.R;
 import net.nightwhistler.pageturner.library.LibraryBook;
-import net.nightwhistler.pageturner.library.LibraryService;
 import net.nightwhistler.pageturner.library.QueryResult;
-import net.nightwhistler.pageturner.library.SqlLiteLibraryService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import android.R.color;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.GridView;
 
-public class BookCaseView extends View {
+public class BookCaseView extends GridView {
 	
-	private Drawable background;
-	private Drawable shelf;
+	private Bitmap background;
+		
+	private int mShelfWidth;
+	private int mShelfHeight;	
 	
-	private static final int WIDTH = 150;
-	private static final int HEIGHT = 150;
+	private QueryResult<LibraryBook> result;	
 	
-	private QueryResult<LibraryBook> result;
-	
-	private static final Logger LOG = LoggerFactory.getLogger(BookCaseView.class);
-	private static final DateFormat DATE_FORMAT = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH);
-	
-	private LibraryBook selectedBook;
+	private LibraryBook selectedBook;	
 	
 	public BookCaseView(Context context, AttributeSet attributes) {
 		super(context, attributes);
-		this.background = getResources().getDrawable(R.drawable.pine);
-		this.shelf = getResources().getDrawable(R.drawable.shelf);
-		
+				
 		this.setFocusableInTouchMode(true);
 		this.setClickable(false);
 		
-		//setBackgroundDrawable( new BookCaseDrawable(9) );
-        
+		final Bitmap shelfBackground = BitmapFactory.decodeResource(context.getResources(),
+				R.drawable.shelf_single);
+		setBackground(shelfBackground);
+		this.setFocusable(true);
+	}
+	
+	public void setBackground(Bitmap background) {
+		this.background = background;
+		
+		mShelfWidth = background.getWidth();
+        mShelfHeight = background.getHeight();
 	}	
 	
 	protected void onClick( int bookIndex ) {
@@ -67,75 +48,24 @@ public class BookCaseView extends View {
 		invalidate();
 	}
 	
-	
-	
-	private void drawSelectedBookInfo( Canvas canvas ) {
-		if ( this.selectedBook != null ) {
-			LOG.debug("Drawing rect");
-			
-			Paint paint = new Paint();			
-			paint.setARGB(200, 0, 0, 0);
-			
-			paint.setStyle(Style.FILL);
-			paint.setAntiAlias(true);
-					
-			int padding = 20;
-		
-			canvas.drawRect(padding, padding, getWidth() - padding, getHeight() -  padding, paint);
-			
-			LayoutInflater inflater = (LayoutInflater) getContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
-			View layout  = inflater.inflate(R.layout.book_details, null)
-				.findViewById(R.id.bookInfoContainer);
-			
-			LOG.debug("Setting with to " + (getWidth() - 60) );
-			layout.setLayoutParams(new ViewGroup.LayoutParams(getWidth() - 60, getHeight() - 60 ));
-			fillBookData(layout); 
-			
-			layout.measure(getWidth(), getHeight());
-			canvas.translate(30, 30);
-			layout.layout(0, 0, getWidth(), getHeight() );
-			
-			LOG.debug(  "Width of view is now: " + layout.getWidth() );
-			
-			layout.draw(canvas);
-			
-		} else {
-			LOG.debug("Not drawing rect, since selected book is null");
-		}
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		final int count = getChildCount();
+        final int top = count > 0 ? getChildAt(0).getTop() : 0;
+        final int shelfWidth = mShelfWidth;
+        final int shelfHeight = mShelfHeight;
+        final int width = getWidth();
+        final int height = getHeight();
+        final Bitmap background = this.background;
+
+        for (int x = 0; x < width; x += shelfWidth) {
+            for (int y = top; y < height; y += shelfHeight) {
+                canvas.drawBitmap(background, x, y, null);
+            }
+        }        
+
+        super.dispatchDraw(canvas);
 	}
-	
-	private void fillBookData(View layout) {
-		
-		ImageView coverView = (ImageView) layout.findViewById(R.id.coverImage);
-		
-		if ( this.selectedBook.getCoverImage() != null ) {			
-			coverView.setImageBitmap( selectedBook.getCoverImage() );
-		} else {			
-			coverView.setImageDrawable( getResources().getDrawable(R.drawable.river_diary));
-		}				
-		
-		TextView titleView = (TextView) layout.findViewById(R.id.titleField);
-		titleView.setText(this.selectedBook.getTitle());		
-		
-		TextView authorView = (TextView) layout.findViewById(R.id.authorField);
-		authorView.setText( "by " + this.selectedBook.getAuthor().getFirstName() + " " + this.selectedBook.getAuthor().getLastName() );
-		
-		if (this.selectedBook.getLastRead() != null && ! this.selectedBook.getLastRead().equals(new Date(0))) {
-			TextView lastRead = (TextView) layout.findViewById(R.id.lastRead);
-			lastRead.setText("Last read: " + DATE_FORMAT.format(this.selectedBook.getLastRead()) );
-		}
-		
-		TextView added = (TextView) layout.findViewById(R.id.addedToLibrary);
-		added.setText( "Added to library: " + DATE_FORMAT.format(this.selectedBook.getAddedToLibrary()) );
-		
-		TextView descriptionView = (TextView) layout.findViewById(R.id.bookDescription);
-		descriptionView.setText(this.selectedBook.getDescription());
-	}
-	
-	
-	
 	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -146,17 +76,6 @@ public class BookCaseView extends View {
 		}
 		
 		return false;
-	}
-	
-	
-	
-	public void fireClick( float x, float y ) {
-		//onClick(findIndexAt(x, y));
-	}
-	
-	
-	
-	
-	
+	}	
 	
 }

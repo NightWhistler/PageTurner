19dacd6805784409119ffbe1318f84273dbf00d9
Joel Pedraza <github@saik0.net>
2012-12-20 10:05:37 -0500

    refactor Dialogs to use DialogFragments

--- src/net/nightwhistler/pageturner/activity/Dialogs.java
+++ src/net/nightwhistler/pageturner/activity/Dialogs.java
@@ -20,43 +20,48 @@ package net.nightwhistler.pageturner.activity;
 
 import net.nightwhistler.pageturner.R;
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.text.Html;
 
 public class Dialogs {
 	
-	public static void showAboutDialog(Context context) {
-		
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(R.string.about);
-		builder.setIcon(R.drawable.page_turner);
+	public static class AboutDialogFragment extends DialogFragment {
+		public static final String TAG = "fragment_dialog_about";
 
-		String version = "";
-		try {
-			version = context.getPackageManager().getPackageInfo(
-					context.getPackageName(), 0).versionName;
-		} catch (NameNotFoundException e) {
-			// Huh? Really?
-		}
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setTitle(R.string.about);
+			builder.setIcon(R.drawable.page_turner);
 
-		String html = "<h2>" + context.getString(R.string.app_name) + " " +  version + "</h2>";
-		html += context.getString(R.string.about_gpl);
-		html += "<br/><a href='http://pageturner-reader.org'>http://pageturner-reader.org</a>";
-
-		builder.setMessage( Html.fromHtml(html));
-
-		builder.setNeutralButton(context.getString(android.R.string.ok), 
-				new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();				
+			String version = "";
+			try {
+				version = getActivity().getPackageManager().getPackageInfo(
+						getActivity().getPackageName(), 0).versionName;
+			} catch (NameNotFoundException e) {
+				// Huh? Really?
 			}
-		});
 
-		builder.show();
+			String html = "<h2>" + getActivity().getString(R.string.app_name) + " " +  version + "</h2>";
+			html += getActivity().getString(R.string.about_gpl);
+			html += "<br/><a href='http://pageturner-reader.org'>http://pageturner-reader.org</a>";
+
+			builder.setMessage( Html.fromHtml(html));
+
+			builder.setNeutralButton(getActivity().getString(android.R.string.ok), 
+					new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();				
+				}
+			});
+
+			return builder.create();
+		}
 	}
-
 }

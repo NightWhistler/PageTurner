c43b0761ecea3f7a7faa08055320c3fa67836d9f
Alex Kuiper <alex@nightwhistler.net>
2012-12-21 20:09:57 +0100

    Revert "Made dialog-fragments retain state. Hopefully fixes #231"
    This reverts commit 0f9f69dee52affb101e052cbee3e65a8156fceac.

--- src/net/nightwhistler/pageturner/utils/DialogFragmentUtils.java
+++ src/net/nightwhistler/pageturner/utils/DialogFragmentUtils.java
@@ -40,25 +40,12 @@ public final class DialogFragmentUtils {
 	 * @return a DialogFragment
 	 */
 	public static DialogFragment fromDialog(final Dialog dialog) {
-		RoboSherlockDialogFragment fragment = new RoboSherlockDialogFragment() {
+		return new RoboSherlockDialogFragment() {
 			@Override
 			public Dialog onCreateDialog(Bundle savedInstanceState) {
 				return dialog;
 			}
-			
-			@Override
-			public void onDestroyView() {
-				
-				if (getDialog() != null && getRetainInstance()) {
-				    getDialog().setOnDismissListener(null);
-				}
-				
-				super.onDestroyView();
-			}
 		};
-		
-		fragment.setRetainInstance(true);
-		return fragment;
 	}
 
 	/**

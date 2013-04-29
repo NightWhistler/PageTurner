ff38962e2cf18bc614eb796ff6790aba4dbefed9
Alex Kuiper <alex@nightwhistler.net>
2013-04-29 13:23:15 +0200

    Now uses new ACRA backend.

--- src/net/nightwhistler/pageturner/PageTurner.java
+++ src/net/nightwhistler/pageturner/PageTurner.java
@@ -24,7 +24,12 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey="dC1sc0tQUVpzdlRRMDNhMFJtbW1UaWc6MQ")
+import static org.acra.ReportField.*;
+
+@ReportsCrashes(formKey = "", // will not be used
+        formUri = "http://acra.pageturner-reader.org/crash",
+        customReportContent = { REPORT_ID, APP_VERSION_CODE, APP_VERSION_NAME, ANDROID_VERSION, BRAND, PHONE_MODEL, BUILD, PRODUCT, STACK_TRACE, LOGCAT, PACKAGE_NAME }
+)
 public class PageTurner extends Application {
 	
 	@Override

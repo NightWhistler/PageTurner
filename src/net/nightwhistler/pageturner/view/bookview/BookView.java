2f32c2d40c0f040319e80511c5ea67f8ccb88b6e
Alex Kuiper <alex@nightwhistler.net>
2013-07-23 09:53:48 +0200

    Extra null check

--- src/net/nightwhistler/pageturner/view/bookview/BookView.java
+++ src/net/nightwhistler/pageturner/view/bookview/BookView.java
@@ -876,7 +876,8 @@ public class BookView extends ScrollView implements LinkTagHandler.LinkCallBack 
 		@Override
 		public void handleTagNode(TagNode node, SpannableStringBuilder builder,
 				int start, int end) {
-			String src = node.getAttributeByName("src");
+
+            String src = node.getAttributeByName("src");
 
 			if (src == null) {
 				src = node.getAttributeByName("href");
@@ -885,6 +886,11 @@ public class BookView extends ScrollView implements LinkTagHandler.LinkCallBack 
 			if (src == null) {
 				src = node.getAttributeByName("xlink:href");
 			}
+
+            if ( src == null ) {
+                return;
+            }
+
 			builder.append("\uFFFC");
 			
 			if (src.startsWith("data:image")) {

d7364035b8a3544902f5bc7eba3e8be9f8ebb78d
Rob Hoelz <rob@hoelz.ro>
2013-09-28 17:57:08 +0200

    bookmarks: Hook add button up to events

--- src/net/nightwhistler/pageturner/activity/AddBookmarkFragment.java
+++ src/net/nightwhistler/pageturner/activity/AddBookmarkFragment.java
@@ -26,12 +26,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockDialogFragment;
 import net.nightwhistler.pageturner.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+class AddBookmarkHandler
+	implements TextView.OnEditorActionListener, View.OnClickListener {
+
+	private Dialog dialog;
+	private int bookIndex;
+	private int bookPosition;
+	private TextView textView;
+
+	private static final Logger LOG = LoggerFactory
+		    .getLogger(AddBookmarkFragment.class);
+
+	AddBookmarkHandler(Dialog dialog, int bookIndex, int bookPosition, TextView textView)
+	{
+		this.dialog       = dialog;
+		this.bookIndex    = bookIndex;
+		this.bookPosition = bookPosition;
+		this.textView     = textView;
+	}
+
+	private void handleAction()
+	{
+	    dialog.dismiss();
+	    LOG.info("    >>> Creating bookmark: " + textView.getText());
+	    LOG.info("    >>> at index:    " + bookIndex);
+	    LOG.info("    >>> at position: " + bookPosition);
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+	{
+		if(actionId == EditorInfo.IME_ACTION_DONE) {
+			handleAction();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		handleAction();
+	}
+}
 
 public class AddBookmarkFragment extends RoboSherlockDialogFragment
 {
@@ -42,25 +87,16 @@ public class AddBookmarkFragment extends RoboSherlockDialogFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-	final Dialog dialog = getDialog();
+	Dialog dialog = getDialog();
 	dialog.setTitle(R.string.add_bookmark);
 
-	View v        = inflater.inflate(R.layout.fragment_add_bookmark, container, false);
-	EditText text = (EditText) v.findViewById(R.id.bookmark_name);
-	text.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-	    @Override
-	    public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-	    {
-		if(actionId == EditorInfo.IME_ACTION_DONE) {
-		    dialog.dismiss();
-		    LOG.info("    >>> Creating bookmark: " + v.getText());
-		    LOG.info("    >>> at index:    " + bookIndex);
-		    LOG.info("    >>> at position: " + bookPosition);
-		    return true;
-		}
-		return false;
-	    }
-	});
+	View v                     = inflater.inflate(R.layout.fragment_add_bookmark, container, false);
+	EditText text              = (EditText) v.findViewById(R.id.bookmark_name);
+	Button addButton           = (Button) v.findViewById(R.id.add_bookmark_button);
+	AddBookmarkHandler handler = new AddBookmarkHandler(dialog, bookIndex, bookPosition, text);
+
+	text.setOnEditorActionListener(handler);
+	addButton.setOnClickListener(handler);
 
 	return v;
     }

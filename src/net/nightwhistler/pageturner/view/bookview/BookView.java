NightWhistler/PageTurner

commit 9dc75038618786d9e4ae64b84a8f6ce7cff2d075

Fixed page-number calculation.

src/net/nightwhistler/pageturner/view/bookview/BookView.java
@@ -1079,6 +1079,8 @@ public class BookView extends ScrollView implements LinkTagHandler.LinkCallBack 
     }
 
 	public int getPageNumberFor( int index, int position ) {
+
+        LOG.debug( "Looking for pageNumber for index=" + index + ", position=" + position );
 		
 		int pageNum = 0;
 		
@@ -1089,24 +1091,45 @@ public class BookView extends ScrollView implements LinkTagHandler.LinkCallBack 
 		}
 		
 		for ( int i=0; i < index; i++ ) {
-			pageNum += pageOffsets.get(i).size();			
+
+            int pages = pageOffsets.get(i).size();
+
+			pageNum += pages;
+
+            LOG.debug("Index " + i + ": pages=" + pages);
 		}
-		
-		List<Integer> offsets;
-		
-		if ( this.strategy.isScrolling() ) {		
-			offsets = pageOffsets.get(index);
-		
-			for ( int i=0; i < offsets.size() && offsets.get(i) < position; i++ ) {
-				pageNum++;
-			}
-		
-		} else {
-			pageNum+= ((FixedPagesStrategy) strategy).getCurrentPage();
-		}		
-		
+
+        List<Integer> offsets = pageOffsets.get(index);
+
+        LOG.debug("Pages before this index: " + pageNum );
+
+        LOG.debug( "Offsets according to spine: " + asString(offsets) );
+
+        if ( this.strategy instanceof FixedPagesStrategy ) {
+            List<Integer> strategyOffsets = ( (FixedPagesStrategy) this.strategy ).getPageOffsets();
+            LOG.debug("Offsets according to strategy: " + asString(strategyOffsets) );
+        }
+
+		for ( int i=0; i < offsets.size() && offsets.get(i) <= position; i++ ) {
+			pageNum++;
+		}
+
+        LOG.debug( "Calculated pageNumber=" + pageNum );
 		return pageNum;
 	}
+
+    private static String asString( List<Integer> offsets ) {
+
+        StringBuilder stringBuilder = new StringBuilder("[ ");
+
+        for ( int i=0; i < offsets.size(); i++ ) {
+            stringBuilder.append( offsets.get( i ) + " " );
+        }
+
+        stringBuilder.append(" ]");
+
+        return stringBuilder.toString();
+    }
 
 	public void setEnableScrolling(boolean enableScrolling) {
 

commit ecfc8e0b3df568949ce82c9d46f78e55b2edf249

Title-page has page-num 0 again.
This is a fix for a bug introduced in
9dc75038618786d9e4ae64b84a8f6ce7cff2d075

src/net/nightwhistler/pageturner/view/bookview/BookView.java
@@ -1080,7 +1080,7 @@ public class BookView extends ScrollView implements LinkTagHandler.LinkCallBack 
 
         LOG.debug( "Looking for pageNumber for index=" + index + ", position=" + position );
 		
-		int pageNum = 0;
+		int pageNum = -1;
 		
 		List<List<Integer>> pageOffsets = spine.getPageOffsets();
 		